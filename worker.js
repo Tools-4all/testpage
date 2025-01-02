751a5faf4f91cb182c7880f8465d2bc3e9d04cb7
Tools-4all <toolsfor4all@gmail.com>
2025-01-02 02:11:41 +0200

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,5 +1,3 @@
-let currentPrompt;
-
 class myPrompt {
     constructor(msg = "") {
         this.msg = msg;
@@ -12,15 +10,15 @@ class myPrompt {
         this.waiting = true;
 
         const view = new Int32Array(sharedBuffer);
+        const responseBuffer = new Uint8Array(sharedBuffer, 4);
 
         // Reset signal
         Atomics.store(view, 0, 0);
 
-        // Wait for main thread to signal with the response
+        // Wait for the main thread to signal the response
         while (Atomics.wait(view, 0, 0));
 
-        // Read response
-        const responseBuffer = new Uint8Array(sharedBuffer, 4);
+        // Decode the response
         const textDecoder = new TextDecoder();
         this.response = textDecoder.decode(responseBuffer).replace(/\0/g, ""); // Remove null terminators
         this.waiting = false;
@@ -31,10 +29,8 @@ class myPrompt {
     }
 }
 
-
 self.addEventListener("message", (event) => {
     const { type, code, sharedBuffer } = event.data;
-    console.log("recived messege", type, code, sharedBuffer)
     if (type === "execute") {
         const customConsole = {
             log: (...args) => self.postMessage({ type: "log", message: args.join(" ") }),
