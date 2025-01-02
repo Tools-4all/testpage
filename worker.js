03bbfa46ffc472d967d8bd7eb2b1e8a4a1087ca1
Tools-4all <toolsfor4all@gmail.com>
2025-01-02 02:16:23 +0200

    Update worker.js

--- worker.js
+++ worker.js
@@ -6,30 +6,17 @@ class myPrompt {
     }
 
     prompt(msg, sharedBuffer) {
-        console.log("new")
         self.postMessage({ type: "prompt", message: msg });
         this.waiting = true;
-
         const view = new Int32Array(sharedBuffer);
-
-        // Reset signal before waiting
         Atomics.store(view, 0, 0);
-
-        // Wait for the main thread to signal
         while (Atomics.wait(view, 0, 0) === "ok");
-
-        // Create a copy of the shared buffer for decoding
         const sharedResponseBuffer = new Uint8Array(sharedBuffer, 4);
         const copiedBuffer = new Uint8Array(sharedResponseBuffer.length);
         copiedBuffer.set(sharedResponseBuffer);
-
-        // Decode the response
         const textDecoder = new TextDecoder();
-        this.response = textDecoder.decode(copiedBuffer).replace(/\0/g, ""); // Remove null terminators
-
-        // Reset signal after processing response
+        this.response = textDecoder.decode(copiedBuffer).replace(/\0/g, "");
         Atomics.store(view, 0, 0);
-
         this.waiting = false;
     }
 
