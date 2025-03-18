c3368871949a3cd98ef3d8d6bd3a8eb1bdd41114
GeneriicName <139624416+GeneriicName@users.noreply.github.com>
2025-03-18 12:28:59 +0200

    1

---
+++ testing.js
@@ -0,0 +1,11 @@
+// Tab1
+console.log("This is a log");
+console.info("This is an info");
+console.warn("This is a warning");
+console.error("This is an error");
+prompt("This is a prompt:");
+Promise(resolve => resolve).then()
+alert()
+window.AbortController.apply()
+
+//this is in visual studio code
