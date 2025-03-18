35cb8883f288abf753d416175a56993f09138d01
GeneriicName <139624416+GeneriicName@users.noreply.github.com>
2025-03-18 14:35:15 +0200

    1

--- worker.js
+++ worker.js
@@ -899,6 +899,20 @@ self.addEventListener("message", (event) => {
             while (Atomics.wait(view, 0, 0) === "ok");
         }
 
+        {
+            let names = ["prompt", "alert", "console", "log", "error", "warn", "info", "debug", "clear", "table", "count", "countReset", "assert", "dir", "dirxml", "timeStamp", "trace", "group", "groupCollapsed", "groupEnd", "profile", "profileEnd", "time", "timeEnd", "timeLog"];
+            let funcs = [customAlert, customPrompt, customConsole, cutomConsole.log, customConsole.error, customConsole.warn, customConsole.info, customConsole.debug, customConsole.clear, customConsole.table, customConsole.count, customConsole.countReset, customConsole.assert, customConsole.dir, customConsole.dirxml, customConsole.timeStamp, customConsole.trace, customConsole.group, customConsole.groupCollapsed, customConsole.groupEnd, customConsole.profile, customConsole.profileEnd, customConsole.time, customConsole.timeEnd, customConsole.timeLog];
+            for (let i = 0; i < names.length; i++) {
+                let name = names[i];
+                let func = funcs[i];
+                Object.defineProperty(func, "name", { value: name });
+                func.valueOf = () => `[native ${name} code]`;
+                func.toString = () => `function ${name}() { [native code] }`;
+                func.toLocaleString = () => `function ${name}() { [native code] }`;
+            }
+
+        }
+
 
         const executeCode = (userCode) => {
             try {
@@ -915,7 +929,6 @@ self.addEventListener("message", (event) => {
                 }
             }
         };
-
         try {
             executeCode(code);
         } catch (e) {
