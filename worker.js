f7e094122d851edecabaaae58c2a605522c05450
GeneriicName <139624416+GeneriicName@users.noreply.github.com>
2025-03-21 09:52:08 +0200

    1

--- worker.js
+++ worker.js
@@ -964,6 +964,7 @@ self.addEventListener("message", (event) => {
                     return;
                 }
                 const tableData = buildTableData(data, columns);
+                console.log(getObjectOrStringForLog([data]));
                 self.postMessage({
                     type: "table",
                     table: tableData,
